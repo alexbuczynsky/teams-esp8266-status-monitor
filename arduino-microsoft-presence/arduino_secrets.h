d1b2f56b500aaee961fa65fc7d654f71d7d13f85
alexbuczynsky <a.buczynsky@kyotocooling.com>
2020-04-05 16:00:32 -0500

    microsoft presence code

---
+++ arduino-microsoft-presence/arduino_secrets.h
@@ -0,0 +1,4 @@
+#define SECRET_SSID "Haus of Adele"
+#define SECRET_PASS "adeletheaussie"
+#define SECRET_OFFICE365_CLIENTID "0f7120fe-24e2-49fc-a492-2d8032e41b68"
+#define SECRET_OFFICE365_DOMAIN "datarumble.com"
